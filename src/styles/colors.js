178050705ad2e92d9f2f8ad58b76a02513a9a589
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-06 12:22:27 -0300

    style(styles): create colors.js

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primaryColor: '#3772FF',
+  secondaryColor: '#36D6AD',
+  tertiaryColor: '#FC7071',
+  tertiaryColorHover: '#FF9D9E', 
+
+  detailsColor100: '#F7F7F7',
+  detailsColor300: '#BCBCBC',
+  detailsColor500: '#737380', 
+}
