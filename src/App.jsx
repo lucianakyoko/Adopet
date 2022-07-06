b2d34fb7c5a65a06839c4d8176ff8c3474938268
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-06 13:32:27 -0300

    feat(src): add initial route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router'
+
 function App() {
   return (
-    <h1>Adopet</h1>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 
