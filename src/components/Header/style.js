ba3389564c2a5690567cf41f6bbc7a7898f31ff7
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-06 14:59:16 -0300

    style(components/Header): remove bg color

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -10,8 +10,6 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  background-color: yellowgreen;
 `;
 
 export const IconsWrapper = styled.div`
