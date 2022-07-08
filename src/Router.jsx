d5dc953b6f7119d2124d9fa043251aab83dcad46
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-08 12:20:39 -0300

    feat(Router): add Registration route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { Registration } from "./pages/Registration";
 
 export function Router() {
   return(
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="cadastro" element={<Registration />}/>
     </Routes>
   )
 }
