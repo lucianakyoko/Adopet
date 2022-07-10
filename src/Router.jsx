1334b973c1365efd8fd3dab29203d8860ea43aca
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-07-10 11:08:26 -0300

    feat(Router): add Animals route

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Registration } from "./pages/Registration";
 import { Login } from './pages/Login';
+import { Animals } from "./pages/Animals";
 
 export function Router() {
   return(
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<Home />}/>
       <Route path="cadastro" element={<Registration />}/>
       <Route path="/login" element={<Login />} />
+      <Route path="/animais" element={<Animals />} />
     </Routes>
   )
 }
